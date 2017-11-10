1971f151f26619abc56cd0517d0fca20d2e0f9d0
olegvasilev1999 <32066946+olegvasilev1999@users.noreply.github.com>
2017-11-10 09:33:27 +0300

    Update 001.cpp

--- 001.cpp
+++ 001.cpp
@@ -33,7 +33,7 @@ int main() {
 		int k = abs(n);
 		int * array = new int[k];
 		if (read_numbers(n, array) && n > 0) {
-		    if 
+	
 			int j = n - 1;
 			for (int i = 0; i < n / 2 ; i++) { 
 				swap(array[i], array[j]);
